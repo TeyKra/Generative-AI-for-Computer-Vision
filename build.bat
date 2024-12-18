4f8b9e20dad15e69686b031f97002df490aadfbf
TeyKra <morgansenechal94@gmail.com>
2024-12-18 20:37:56 +0100

    V2

--- build.bat
+++ build.bat
@@ -1,13 +1,23 @@
 @echo off
-echo [INFO] Setting up the virtual environment...
-python -m venv venv
+set "PROJECT_DIR=%~dp0"
+set "VENV_DIR=%PROJECT_DIR%\venv"
+set "REQUIREMENTS_FILE=%PROJECT_DIR%\requirements.txt"
 
-echo [INFO] Activating the virtual environment...
-call venv\Scripts\activate
+IF EXIST "%VENV_DIR%" (
+    echo [INFO] Erasing existing venv...
+    rmdir /s /q "%VENV_DIR%"
+)
+
+echo [INFO] Creating venv...
+python -m venv "%VENV_DIR%"
+call "%VENV_DIR%\Scripts\activate"
+
+echo [INFO] Upgrading pip...
+python -m pip install --upgrade pip
 
 echo [INFO] Installing dependencies...
-pip install --upgrade pip
-pip install -r requirements.txt
+python -m pip install -r "%REQUIREMENTS_FILE%"
 
-echo [INFO] Virtual environment setup complete!
+call deactivate
+echo [INFO] venv created. Press Enter to exit.
 pause
